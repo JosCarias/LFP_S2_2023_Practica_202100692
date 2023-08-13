3e048103b675c652a0453f0b60220ecac73a433a
Josue Ricardo Carias Ordoñez <3661044620101@ingenieria.usac.edu.gt>
2023-08-13 01:48:52 -0600

    sabado 12

--- cargarInstrucciones.py
+++ cargarInstrucciones.py
@@ -1,35 +1,70 @@
 def getCargarInstrucciones():
-    from cargarInventario import leerArchivosInv
+    from cargarInventario import imprimirInventario, leerArchivosInv
     from menuPrincipal import getMenuPrincipal
+    inventario ={}
+
     print("---------------------------------------------------")
-    print("Instrucciones de movimientos")
+    print("Cargar Instrucciones")
     print("---------------------------------------------------")
-    print("Ingrese la direccion relativa del archivo")
-    ruta_mov=input()
-    print("Ingrese la direccion relativa del archivo")
-    ruta_invetario=input()
-    try:
-        with open(ruta_mov, "r") as archivo:
-            for linea in archivo:
-                instruccion, detalles = linea.strip().split(' ', 1)
-                if instruccion == 'agregar_stock':
-                    nombre, cantidad, ubicacion = detalles.split(';')
-                    cantidad = int(cantidad)
-                    agregar(leerArchivosInv(ruta_invetario), nombre, cantidad, ubicacion)
-    except FileNotFoundError:
-        print(f"El archivo {ruta_mov} no fue encontrado.")
-        getCargarInstrucciones()
-    except ValueError:
-        print("Error: El formato del archivo es incorrecto.")
-        getCargarInstrucciones()
+
+    print("Ingrese la direccion relativa del archivo inventario")
+    rutaInventario=input()
+    print("Ingrese la direccion relativa del archivo movimiento")
+    rutaMovimiento=input()
+
+    inventario=leerArchivosInv(rutaInventario)
+    inventario=cargarCambios(rutaMovimiento,inventario)
+    imprimirInventario(inventario)
+    actualizarInventario(rutaInventario, inventario)
+
+
     print("---------------------------------------------------")
     print("Presiciones cualquier tecla para continuar")
     input()
     getMenuPrincipal()
 
-def agregar(inventario, nombre, cantidad, ubicacion):
-    if nombre in inventario and ubicacion in inventario[nombre]:
-        inventario[nombre][ubicacion]['cantidad'] += cantidad
-    else:
-        print(f"Error: El producto '{nombre}' no existe en la ubicación '{ubicacion}'.")
+def cargarCambios(rutaMovimiento, ubicaciones):
+    try:
+        with open(rutaMovimiento, encoding="utf-8") as file:
+            for linea in file:
+                instruccion, informacion = linea.strip().split(" ")
+                producto, cantidad, ubicacion = informacion.split(";")
+                if instruccion == "agregar_stock":
+                    if ubicacion not in ubicaciones:
+                        print("Esa ubicacion no existe")
+                        continue
+                    if producto not in ubicaciones[ubicacion]:
+                        print("Ese producto no existe en esa ubicacion")
+                        continue
+                    ubicaciones[ubicacion][producto]['cantidad'] += float(cantidad)   
+                elif instruccion == "vender_producto":
+                    if ubicacion not in ubicaciones:
+                        print("Esa ubicacion no existe")
+                    if ubicacion not in ubicaciones or producto not in ubicaciones[ubicacion]:
+                        print("Ese producto no esta en esa ubicacion") 
+                    cantidadVender = float(cantidad)
+                    cantidadDisponible = ubicaciones[ubicacion][producto]['cantidad']
+                    if cantidadVender > cantidadDisponible:
+                        print("Error: La cantidad de producto no está disponible")
+                    else:
+                        ubicaciones[ubicacion][producto]['cantidad'] -= float(cantidadVender)
+        return ubicaciones
+    except FileNotFoundError:
+        print("El archivo no fue encontrado.")
+        getCargarInstrucciones()
+    except IOError:
+        print("Error al leer el archivo.")
+        getCargarInstrucciones()
 
+def actualizarInventario(rutaInventario, inventario):
+    try:
+        with open(rutaInventario, "w") as archivo_inv:
+            for ubicacion, ubicaciones in inventario.items():
+                for producto, detalles in ubicaciones.items():
+                    cantidad = detalles["cantidad"]
+                    precio = detalles["precio"]
+                    archivo_inv.write(f"crear_producto {producto};{cantidad};{precio};{ubicacion}\n")
+        print("Archivo de inventario actualizado correctamente.")
+    except IOError:
+        print("Error al escribir en el archivo de inventario.")
+        getCargarInstrucciones()
--- cargarInventario.py
+++ cargarInventario.py
@@ -1,30 +1,39 @@
 import json
 def getCargarInicial():
     from menuPrincipal import getMenuPrincipal
+
     print("---------------------------------------------------")
     print("Inventario Principal")
     print("---------------------------------------------------")
+
     print("Ingrese la direccion relativa del archivo")
     ruta=input()
     imprimirInventario(leerArchivosInv(ruta))
+    
     print("---------------------------------------------------")
     print("Presiciones cualquier tecla para continuar")
     input()
     getMenuPrincipal()
 
 def leerArchivosInv(ruta):
-    inventario={}
+    ubicaciones ={}
     try:
-        with open(ruta, "r") as archivo_inv:
-            for linea in archivo_inv:
-                instruccion, detalles = linea.strip().split(' ', 1)
-                nombre, cantidad, precio_unitario, ubicacion = detalles.split(';')
-                inventario[nombre] = {
-                    'cantidad': float(cantidad),
-                    'precio_unitario': float(precio_unitario),
-                    'ubicacion': ubicacion
-                }
-        return inventario
+        with open(ruta, encoding="utf-8") as file:
+            for linea in file:
+                producto, cantidad, precio, ubicacion= linea.strip().split(" ")[1].split(";")
+                if ubicacion in ubicaciones:
+                    ubicaciones[ubicacion][producto]={
+                        "cantidad": float(cantidad),
+                        "precio": float(precio)
+                    }
+                else:
+                    ubicaciones[ubicacion]={
+                        producto:{
+                            "cantidad": float(cantidad),
+                            "precio": float(precio)
+                        }
+                    }
+        return ubicaciones
     except FileNotFoundError:
         print("El archivo no fue encontrado.")
         getCargarInicial()
